62a01af30be6265144dbdc4d56fcb58264e8d96f
MerleESelf <merleself@gmail.com>
2022-05-25 16:06:15 -0400

    routes

---
+++ server/api/plant.js
@@ -0,0 +1,15 @@
+
+   
+const router = require('express').Router()
+const {Plant} = require('../db/models')
+module.exports = router
+
+// get route for all plants
+router.get('/', async (req, res, next) => {
+  try {
+    const plants = await Plant.findAll()
+    res.json(plants)
+  } catch (err) {
+    next(err)
+  }
+})
