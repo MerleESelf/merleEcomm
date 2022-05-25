a158b2176411f8410dd27b63a52380bc5153399a
MerleESelf <merleself@gmail.com>
2022-05-25 16:01:10 -0400

    api

---
+++ server/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+module.exports = router
+
+//user api 
+router.use('/user', require('./user'))
+
+// plant api
+router.use('/plant', require('./plant'))
+
+//order api      
+router.use('/order', require('./order'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
