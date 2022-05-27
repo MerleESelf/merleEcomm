5bc65d8049064719cad0d4b955c54b756c0c5967
MerleESelf <merleself@gmail.com>
2022-05-27 14:23:02 -0400

    redux stuff

--- ecommfront/src/store/allPlants.js
+++ ecommfront/src/store/allPlants.js
@@ -1,33 +1,30 @@
 import axios from 'axios'
-import history from '../history'
 
 //action constants
 
 const GET_ALL_PLANTS = 'GET_ALL_PLANTS'
 
 // action creators
-
 const fetchAllPlants = plants => ({
-  type: GET_ALL_PLANTS,
-  plants
+    type: GET_ALL_PLANTS,
+    plants
 })
 
 // thunks
 export const getAllPlants = () => {
-  return async dispatch => {
-    const {data} = await axios.get('/api/plants')
-    const plants = data
-    dispatch(fetchAllPlants(plants))
-  }
+    return async dispatch => {
+        const { data } = await axios.get('/api/plants')
+        const plants = data
+        dispatch(fetchAllPlants(plants))
+    }
 }
 
 // reducer
-
-export default function(state = [], action) {
-  switch (action.type) {
-    case GET_ALL_PLANTS:
-      return action.plants
-    default:
-      return state
-  }
+export default function (state = [], action) {
+    switch (action.type) {
+        case GET_ALL_PLANTS:
+            return action.plants
+        default:
+            return state
+    }
 }
---
+++ ecommfront/src/store/routes.js
@@ -0,0 +1,65 @@
+import React, {Component, Fragment} from 'react'
+import {connect} from 'react-redux'
+import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+// import { Login, Signup } from './components/AuthForm';
+import Home from './components/Home';
+// import {me} from './store'
+import AllPlants from "./components/AllPlants"
+import SinglePlant from './components/SinglePlant';
+
+
+/**
+ * COMPONENT
+ */
+class Routes extends Component {
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
+
+  render() {
+    // const {isLoggedIn} = this.props
+
+    return (
+      <div>
+        {/* {isLoggedIn ? (
+          <Switch>
+            <Route path="/home" component={Home} />
+            {/* <Route exact path="/plant" component={AllPlants} />
+            <Route exact path="/plant/:id" component={SinglePlant} /> */}
+            {/* <Redirect to="/home" />
+          </Switch> */}
+        {/* ) : ( */} 
+   
+            <Route path='/' exact component={ Home } />
+            <Route exact path="/plant" component={AllPlants} />
+            <Route exact path="/plant/:id" component={SinglePlant} />
+            {/* <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} /> */}
+     
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+    isLoggedIn: !!state.auth.id
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me())
+    }
+  }
+}
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(Routes))
