cb83fd45f8ebf890f5584ad611dc6e5ab3ac1e36
MerleESelf <merleself@gmail.com>
2022-06-03 19:09:03 -0400

    updated auth slice with RTK

--- ecommfront/src/store/authSlice.js
+++ ecommfront/src/store/authSlice.js
@@ -1,78 +1,79 @@
 import axios from 'axios'
 import history from '../history'
-import {createSlice} from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+
+// thunks
+export const me = createAsyncThunk(
+    'auth/me',
+    async () => {
+        try {
+            const token = window.localStorage.getItem(TOKEN)
+            if (token) {
+                const res = await axios.get('/auth/me', {
+                    headers: {
+                        authorization: token
+                    }
+                })
+            }
+            return res.data
+        } catch (err) {
+            console.log(err)
+        }
+    }
+)
+
+export const authenticate = createAsyncThunk(
+    'auth/authenticate',
+    async userData => {
+        try {
+            const { username, userpassword, method } = userData
+            const res = await axios.post(`/auth/${method}`, { username, userpassword })
+            window.localStorage.setItem(TOKEN, res.data.token)
+            dispatch(me())
+        } catch (authError) {
+            console.log(authError)
+        }
+    }
+)
+
+export const logout = createAsyncThunk(
+    'auth/logout',
+    () => {
+        window.localStorage.removeItem(TOKEN)
+        history.push('/login')
+    }
+)
 
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        value: {}
+        value: {},
+        status: 'idle',
+        error: null
     },
-    reducers:  {
-        setAuth: (state = initialState, action) => {
-            state.value = action.payload
-        }
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(me.pending, (state, action) => {
+            state.status = 'loading'
+        })
+        builder.addCase(me.fulfilled, (state, action) => {
+            state.status = 'succeded'
+            return action.payload
+        })
+        builder.addCase(me.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        })
+        builder.addCase(logout.pending, (state, action) => {
+            state.status = 'loading'
+        })
+        builder.addCase(logout.fulfilled, (state, action) => {
+            state.status = 'succeded'
+            state.value = {}
+        })
     }
 })
 
 export const { setAuth } = authSlice.actions
 export default authSlice.reducer
-
-
-
-// const TOKEN = 'token'
-
-// /**
-//  * ACTION TYPES
-//  */
-// const SET_AUTH = 'SET_AUTH'
-
-// /**
-//  * ACTION CREATORS
-//  */
-// const setAuth = auth => ({type: SET_AUTH, auth})
-
-// /**
-//  * THUNK CREATORS
-//  */
-// export const me = () => async dispatch => {
-//   const token = window.localStorage.getItem(TOKEN)
-//   if (token) {
-//     const res = await axios.get('/auth/me', {
-//       headers: {
-//         authorization: token
-//       }
-//     })
-//     return dispatch(setAuth(res.data))
-//   }
-// }
-
-// export const authenticate = (username, password, method) => async dispatch => {
-//   try {
-//     const res = await axios.post(`/auth/${method}`, {username, password})
-//     window.localStorage.setItem(TOKEN, res.data.token)
-//     dispatch(me())
-//   } catch (authError) {
-//     return dispatch(setAuth({error: authError}))
-//   }
-// }
-
-// export const logout = () => {
-//   window.localStorage.removeItem(TOKEN)
-//   history.push('/login')
-//   return {
-//     type: SET_AUTH,
-//     auth: {}
-//   }
-// }
-
-// /**
-//  * REDUCER
-//  */
-// export default function auth(state = {}, action) {
-//   switch (action.type) {
-//     case SET_AUTH:
-//       return action.auth
-//     default:
-//       return state
-//   }
-// }
