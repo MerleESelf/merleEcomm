5ed3058dc25a1eed74f3fc83d305ca8c3c95e624
MerleESelf <merleself@gmail.com>
2022-06-03 18:25:00 -0400

    finalized redux redo

--- ecommfront/src/store/plantsSlice.js
+++ ecommfront/src/store/plantsSlice.js
@@ -1,46 +1,50 @@
 import axios from 'axios'
-import {createSlice} from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+// thunks
+export const getAllPlants = createAsyncThunk(
+    'plants/getAllPlants',
+    async () => {
+        try {
+            const { data } = await axios.get('/api/plants')
+            const plants = data
+            return plants
+        } catch (err) {
+            console.log(err)
+        }
+
+    })
 
 export const plantsSlice = createSlice({
     name: 'allPlants',
     initialState: {
-        value: []
+        value: [],
+        status: 'idle',
+        error: null
     },
-    reducers:  {
-        fetchPlants: (state = initialState , action) => {
-            state.value =  action.payload
+    reducers: {
+        fetchPlants: (state, action) => {
+            state.value = action.payload
         }
+    },
+    extraReducers(builder) {
+        builder.addCase(getAllPlants.pending, (state, action) => {
+            state.status = 'loading'
+        })
+        builder.addCase(getAllPlants.fulfilled, (state, action) => {
+            state.status = 'succeded'
+            return action.payload
+        })
+        builder.addCase(getAllPlants.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        })
+
     }
 })
+
+
 
 export const { fetchPlants } = plantsSlice.actions
 export default plantsSlice.reducer
 
-//action constants
-
-// const GET_ALL_PLANTS = 'GET_ALL_PLANTS'
-
-// // action creators
-// const fetchAllPlants = plants => ({
-//     type: GET_ALL_PLANTS,
-//     plants
-// })
-
-// // thunks
-// export const getAllPlants = () => {
-//     return async dispatch => {
-//         const { data } = await axios.get('/api/plants')
-//         const plants = data
-//         dispatch(fetchAllPlants(plants))
-//     }
-// }
-
-// // reducer
-// export default function (state = [], action) {
-//     switch (action.type) {
-//         case GET_ALL_PLANTS:
-//             return action.plants
-//         default:
-//             return state
-//     }
-// }
