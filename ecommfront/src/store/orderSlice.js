96f7818c1c4e06875a9c7a240c30730480c39ae0
MerleESelf <merleself@gmail.com>
2022-06-06 13:16:38 -0400

    updates to order slice

--- ecommfront/src/store/orderSlice.js
+++ ecommfront/src/store/orderSlice.js
@@ -1,67 +1,69 @@
 import axios from 'axios';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import CartDetail from '../../../server/db/models/CartDetail';
 
-export const removedPlantThunk = (userId, orderId, plantId) => async (dispatch) => {
-	try {
-		const { data: order } = await axios.delete(
-			`/api/order/${userId}/${orderId}/${plantId}`
-		)
-	} catch (error) {
-		console.error(error);
-	}
-};
+export const removedPlantThunk = createAsyncThunk(
+    'order/removePlant',
+    async (plantToRemove) => {
+        try {
+            const { userId, orderId, plantId } = plantToRemove
+            const { data: order } = await axios.delete(
+                `/api/order/${userId}/${orderId}/${plantId}`
+            )
+            return order
+        } catch (error) {
+            console.error(error);
+        }
+    })
 
-export const updateCartAdd = (userId, plantId) => async (dispatch) => {
-	try {
-		const { data: cartDetailsPlant } = await axios.get(
-			`/api/order/${userId}/${plantId}`
-		);
-		
-	} catch (error) {
-		console.error(error);
-	}
-};
+export const updateCartAdd = createAsyncThunk(
+    'order/updateCartAdd',
+    async cartDetails => {
+        try {
+            const { userId, plantId } = cartDetails
+            const { data: cartDetailsPlant } = await axios.get(
+                `/api/order/${userId}/${plantId}`
+            );
+            return cartDetailsPlant
+        } catch (error) {
+            console.error(error);
+        }
+    })
 
 
-export const fetchCompleteOrder = (userId, orderId) => {
-	return async (dispatch) => {
-		const { data } = await axios.get(`/api/order/${userId}/${orderId}`);
-		const order = data;
-        return order
-	};
-};
+export const fetchCompleteOrder = createAsyncThunk(
+    'order/fetchCompleteOrder',
+    async (orderDetails) => {
+        try {
+            const { data } = await axios.get(`/api/order/${userId}/${orderId}`);
+            const order = data;
+            return order
+        } catch (error) {
+            console.log(error)
+        }
+    }
+)
 
-export const fetchOpenOrder = (userId) => {
-	return async (dispatch) => {
-		const { data } = await axios.get(`/api/order/${userId}`);
-		return data 
-	};
-	
-};
+export const fetchOpenOrder = createAsyncThunk(
+    'order/fetchOpenOrder',
+    async (userId) => {
+        try {
+            const { data } = await axios.get(`/api/order/${userId}`);
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    })
 
 
 
 export const editPlantQtyThunk = (userId, orderId, plantId, plant) => {
-  return async (dispatch) =>  {
-    const { data: order} = await axios.put(`/api/order/${userId}/${orderId}/${plantId}`, plant); 
-  };
+    return async (dispatch) => {
+        const { data: order } = await axios.put(`/api/order/${userId}/${orderId}/${plantId}`, plant);
+    };
 
 };
 
-
-// export const checkOutThunk = (userId, orderId, history) => {
-// 	return async (dispatch) => {
-// 		try {
-// 			//order complete
-// 			const { data } = await axios.put(`/api/order/${userId}/${orderId}`);
-// 			// dispatch(orderComplete(data));
-// 			//subtract quantity
-// 			history.push('/thankyou');
-// 		} catch (err) {
-// 			console.log(err);
-// 		}
-// 	};
-// }
 
 export const orderSlice = createSlice({
     name: 'order',
@@ -76,14 +78,14 @@ export const orderSlice = createSlice({
         builder.addCase(.pending, (state, action) => {
             state.status = 'loading'
         })
-        builder.addCase(.fulfilled, (state, action) => {
-            state.status = 'succeded'
-            return action.payload
-        })
-        builder.addCase(.rejected, (state, action) => {
-            state.status = 'failed'
-            state.error = action.error.message
-        })
+builder.addCase(.fulfilled, (state, action) => {
+    state.status = 'succeded'
+    return action.payload
+})
+builder.addCase(.rejected, (state, action) => {
+    state.status = 'failed'
+    state.error = action.error.message
+})
 
     }
 })
