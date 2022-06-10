322bd0e036fb07e04fc9c8d3969fbc0b6e0b0b2d
MerleESelf <merleself@gmail.com>
2022-06-10 14:08:08 -0400

    plant slice updated with new thunk and reducer

--- ecommfront/src/store/plantsSlice.js
+++ ecommfront/src/store/plantsSlice.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-// thunks
+// thunk for plants via paginated api route 
 export const getAllPlants = createAsyncThunk(
     'plants/getAllPlants',
-    async () => {
+    async (query) => {
         try {
-            const { data } = await axios.get('/api/plant')
+            const { data } = await axios.get(`/api/plant${query}`)
             const plants = data
             return plants
         } catch (err) {
@@ -15,25 +15,54 @@ export const getAllPlants = createAsyncThunk(
 
     })
 
+// thunk for total number of plants in DB via paginated api route 
+export const getTotalPlantCount = createAsyncThunk(
+    'plants/getTotalPlantCount',
+    async (query) => {
+        try {
+            const { data } = await axios.get(`/api/plant${query}`)
+            const totalCount = data
+            return totalCount
+        } catch (err) {
+            console.log(err)
+        }
+
+    })
+
+
 export const plantsSlice = createSlice({
     name: 'allPlants',
     initialState: {
         value: [],
+        valueCount: [],
         status: 'idle',
         error: null
     },
     reducers: {
     },
     extraReducers(builder) {
-        builder.addCase(getAllPlants.pending, (state, action) => {
+        //reducers for all plants 
+        builder.addCase(getAllPlants.pending, (state) => {
             state.status = 'loading';
         })
         builder.addCase(getAllPlants.fulfilled, (state, action) => {
-            state.status = 'succeded'; 
+            state.status = 'succeded';
             state.value = action.payload
         })
         builder.addCase(getAllPlants.rejected, (state, action) => {
-            state.status = 'failed'; 
+            state.status = 'failed';
+            state.error = action.error.message
+        })
+        // reducers for get total plant count 
+        builder.addCase(getAllPlants.pending, (state) => {
+            state.status = 'loading';
+        })
+        builder.addCase(getAllPlants.fulfilled, (state, action) => {
+            state.status = 'succeded';
+            state.valueCount = action.payload
+        })
+        builder.addCase(getAllPlants.rejected, (state, action) => {
+            state.status = 'failed';
             state.error = action.error.message
         })
 
