2fcfcc7704f70c6383d8881407be26ddc33882f3
MerleESelf <merleself@gmail.com>
2022-06-23 17:18:47 -0400

    end of page game

--- ecommfront/src/components/PaginationBar.js
+++ ecommfront/src/components/PaginationBar.js
@@ -1,37 +1,25 @@
 import Pagination from 'react-bootstrap/Pagination'
 import React from 'react'
 
-
-
 const PaginationBar = (props) => {
-  // destructure props 
   const { totalPages, curPage, nextPage, previousPage, changePage } = props
 
-  // var to denote which page is active 
-  let active = curPage
-  // arr to hold num of pages needed 
-  const pages = [];
-
   // should trigger the parent call back change handler 
-  const handleChange = (event, data) => {
-
-    console.log("444444", data)
-    console.log(event.target.attributes[1].value, ' event.target in PB')
+  const handleChange = (event) => {
     const newPage = event.target.attributes[1].value
     changePage(newPage)
-    active = Number(newPage)
-
+  }
+  let active = curPage
+  const isPageActive = (pageNum) => {
+    return active === pageNum ? true : false
   }
 
-  // ****** try map tomorrow 
-  // push pag. item into arr to be given to parent pagination element 
-  for (let i = 1; i <= totalPages; i++) {
-    pages.push(
-      <Pagination.Item value={i} onClick={handleChange} key={i} active={i === active}>
-        {i}
-      </Pagination.Item>,
-    )
-  }
+  const pages = totalPages.map((pageNum) => {
+    return (
+      <Pagination.Item value={pageNum} onClick={handleChange} key={pageNum}  >
+        {pageNum}
+      </Pagination.Item>)
+  })
 
   return (
     <div>
